e56627871fdf3a800d66ed47651d33c86e914667
melu <m3l04r@gmail.com>
2013-07-10 14:29:50 +0000

    primer commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    render :text => "hue hue"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prueba::Application.config.secret_key_base = 'd4ee0001357616158f7ce11068bf22c2f30a3dde21b8ff3beead3ea58cb199f4ec8a8a3d9a56bc51e092dd643bce976c7e5f61104eb092dbae76992976c19588'
